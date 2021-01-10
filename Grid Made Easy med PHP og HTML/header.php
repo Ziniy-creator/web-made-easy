d3966296b4ad995c64e20ebd1066b903c8d9a257
Ziniy-creator <69847425+Ziniy-creator@users.noreply.github.com>
2021-01-10 22:50:15 +0100

    Update header.php

--- Grid Made Easy med PHP og HTML/header.php
+++ Grid Made Easy med PHP og HTML/header.php
@@ -3,20 +3,9 @@
 <nav>
     <ul>
         <li><a href="#">Home</a></li>
-        <li><a href="#">Auctions</a></li>
+        <li><a href="#">About</a></li>
         <li><a href="#">Sell</a></li>
         <li><a href="#">Buy</a></li>
         <li><a href="#">About</a></li>
-        <li>
-            <a href="#">Login
-                <nav>
-                    <ul>
-                        <li><a href="#">Account Settings</a></li>
-                        <li><a href="#">History</a></li>
-                        <li><a href="#">Logout</a></li>
-                    </ul>
-                </nav>
-            </a>
-        </li>
     </ul>
 </nav>
