250f42ec5fb560c1850e8e435d4f5d1f525192de
Ziniy-creator <69847425+Ziniy-creator@users.noreply.github.com>
2021-01-13 09:55:37 +0100

    Added new stuff

--- Grid Made Easy med PHP og HTML/index.php
+++ Grid Made Easy med PHP og HTML/index.php
@@ -16,17 +16,31 @@
     <div class="container"> <!-- grid container klasse, boks uden om alt indhold -->
         <nav><?php include("header.php") ?></nav> <!-- Includere Header med link til div css -->
         
-        <main>Main content</main>
-        <div id="box1">Box 1</div>
-        <div id="box2">Box 2</div>
-        <div id="box3">Box 3</div>
-        <div id="box4">Box 4</div>
-        <div id="box5">Box 5</div>
-        <div id="box6">Box 6</div>
-        <div id="box7">Box 7</div>
+        <main>Indhold</main>
+        <div id="box1">
+        <h1>Heading 1</h1>
+        Box 1</div>
+        <div id="box2">
+        <h2>Heading 2</h2>Box 2</div>
+        <div id="box3">
+        <h3>Heading 3</h3>Box 3</div>
+        <div id="box4">
+        <h4>Heading 4</h4>Box 4</div>
+        <div id="box5">
+        <h5>Heading 5</h5>Box 5</div>
+        <div id="box6">
+        <h6>Heading 6</h6>Box 6</div>
+        <div id="box7">
+        <p>Paragraph
+        
+        </p>Box 7</div>
         <div id="box8">Box 8</div>
         <footer>Footer</footer>
     </div>
+
+    <footer>
+    <a href="#">Link</a>
+    </footer>
 
 </body>
 </html>
