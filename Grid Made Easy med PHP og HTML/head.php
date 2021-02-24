f25ea8979c5e5d3d5738f4f298721b6ba1772930
Ziniy-creator <69847425+Ziniy-creator@users.noreply.github.com>
2021-02-24 19:02:50 +0100

    from php to html

--- Grid Made Easy med PHP og HTML/head.php
+++
@@ -1,7 +0,0 @@
-<head>
-<?php include("dbconnection.php")?> <!-- includer database opkoblings fil, ikke nødvendigt -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Monargo - Demo 1</title>
-    <link rel="stylesheet" href="/css/style.css"> <!-- link til css -->
-</head>
