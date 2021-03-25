e85df9de7b74f3ea87101c7f21bc00d40b2c614a
Ziniy-creator <69847425+Ziniy-creator@users.noreply.github.com>
2021-03-26 00:06:30 +0100

    lommeregner

--- grid opgaver/lommeregner/script.js
+++ grid opgaver/lommeregner/script.js
@@ -1,12 +1,127 @@
-let large = document.getElementById("box1");
-let thumbnails = document.querySelectorAll(".thumbnails");
-
-
-for(let index = 0; index < thumbnails.length; index++){
-    thumbnails[index].addEventListener("click", (picture)=>{
-
-        large.setAttribute("src", picture.target.src);
-
-    });
-
+let calculate = (n1, operator, n2) => {
+  let firstNum = parseFloat(n1)
+  let secondNum = parseFloat(n2)
+  if (operator === 'add') return firstNum + secondNum
+  if (operator === 'subtract') return firstNum - secondNum
+  if (operator === 'multiply') return firstNum * secondNum
+  if (operator === 'divide') return firstNum / secondNum
 }
+
+let getKeyType = key => {
+  let { action } = key.dataset
+  if (!action) return 'number'
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) return 'operator'
+  return action
+}
+
+let createResultString = (key, displayedNum, state) => {
+  let keyContent = key.textContent
+  let keyType = getKeyType(key)
+  let {
+    firstValue,
+    operator,
+    modValue,
+    previousKeyType
+  } = state
+
+  if (keyType === 'number') {
+    return displayedNum === '0' ||
+      previousKeyType === 'operator' ||
+      previousKeyType === 'calculate'
+      ? keyContent
+      : displayedNum + keyContent
+  }
+
+  if (keyType === 'decimal') {
+    if (!displayedNum.includes('.')) return displayedNum + '.'
+    if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.'
+    return displayedNum
+  }
+
+  if (keyType === 'operator') {
+    return firstValue &&
+      operator &&
+      previousKeyType !== 'operator' &&
+      previousKeyType !== 'calculate'
+      ? calculate(firstValue, operator, displayedNum)
+      : displayedNum
+  }
+
+  if (keyType === 'clear') return 0
+
+  if (keyType === 'calculate') {
+    return firstValue
+      ? previousKeyType === 'calculate'
+        ? calculate(displayedNum, operator, modValue)
+        : calculate(firstValue, operator, displayedNum)
+      : displayedNum
+  }
+}
+
+let updateCalculatorState = (key, calculator, calculatedValue, displayedNum) => {
+  let keyType = getKeyType(key)
+  let {
+    firstValue,
+    operator,
+    modValue,
+    previousKeyType
+  } = calculator.dataset
+
+  calculator.dataset.previousKeyType = keyType
+
+  if (keyType === 'operator') {
+    calculator.dataset.operator = key.dataset.action
+    calculator.dataset.firstValue = firstValue &&
+      operator &&
+      previousKeyType !== 'operator' &&
+      previousKeyType !== 'calculate'
+      ? calculatedValue
+      : displayedNum
+  }
+
+  if (keyType === 'calculate') {
+    calculator.dataset.modValue = firstValue && previousKeyType === 'calculate'
+      ? modValue
+      : displayedNum
+  }
+
+  if (keyType === 'clear' && key.textContent === 'AC') {
+    calculator.dataset.firstValue = ''
+    calculator.dataset.modValue = ''
+    calculator.dataset.operator = ''
+    calculator.dataset.previousKeyType = ''
+  }
+}
+
+let updateVisualState = (key, calculator) => {
+  let keyType = getKeyType(key)
+  Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+
+  if (keyType === 'operator') key.classList.add('is-depressed')
+  if (keyType === 'clear' && key.textContent !== 'AC') key.textContent = 'AC'
+  if (keyType !== 'clear') {
+    let clearButton = calculator.querySelector('[data-action=clear]')
+    clearButton.textContent = 'CE'
+  }
+}
+
+let calculator = document.querySelector('.calculator')
+let display = calculator.querySelector('.calculator__display')
+let keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', e => {
+  if (!e.target.matches('button')) return
+  let key = e.target
+  let displayedNum = display.textContent
+  let resultString = createResultString(key, displayedNum, calculator.dataset)
+
+  display.textContent = resultString
+  updateCalculatorState(key, calculator, resultString, displayedNum)
+  updateVisualState(key, calculator)
+})
+
