9158c63ce54b356b49ee8d684d8d87ae2beb4389
Ziniy-creator <69847425+Ziniy-creator@users.noreply.github.com>
2021-05-06 12:01:44 +0200

    tt

---
+++ galleri/script.js
@@ -0,0 +1,12 @@
+let large = document.getElementById("box1");
+let thumbnails = document.querySelectorAll(".thumbnails");
+
+
+for(let index = 0; index < thumbnails.length; index++){
+    thumbnails[index].addEventListener("click", (picture)=>{
+
+        large.setAttribute("src", picture.target.src);
+
+    });
+
+}
