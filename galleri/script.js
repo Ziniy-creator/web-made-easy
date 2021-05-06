ecb95eac63cf6d40b71f778107a3089c06d6aeb7
Ziniy-creator <69847425+Ziniy-creator@users.noreply.github.com>
2021-05-06 12:18:55 +0200

    tt

--- galleri/script.js
+++ galleri/script.js
@@ -5,7 +5,8 @@ let thumbnails = document.querySelectorAll(".thumbnails");
 for(let index = 0; index < thumbnails.length; index++){
     thumbnails[index].addEventListener("click", (picture)=>{
 
-        large.setAttribute("src", picture.target.src);
+       large.setAttribute("src", picture.target.dataset.src);
+        console.log(large);
 
     });
 
