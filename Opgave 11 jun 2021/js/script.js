0a312c9fdf7e94d69da2092e4635b75abc845766
Ziniy-creator <69847425+Ziniy-creator@users.noreply.github.com>
2021-06-13 12:21:49 +0200

    tt

---
+++ Opgave 11 jun 2021/js/script.js
@@ -0,0 +1,44 @@
+//Gem  panel klasse og knappermed classe btn
+let panel = document.querySelectorAll(".panel");
+let btn = document.querySelectorAll(".accordion");
+
+for (let index = 0; index < panel.length; index++) {
+    btn[index].addEventListener("click", (event) => {
+        // Kalder panel event
+        console.log(event);
+        
+        panel[index].classList.toggle("active");
+        //tilføjer active til knapper
+       
+        btn[index].classList.toggle("btnactive");
+
+    });
+}
+
+
+function formValidate(){
+
+
+    if (document.getElementById("name").value.length > 5){
+      
+      
+    if ( document.getElementById("email").value.length > 7){
+    
+        console.log("email er valideret");
+       
+    }
+      
+        console.log("navn er valideret");
+        
+        return true;
+    }
+
+    else{
+    
+        console.log("falsk")
+        return false;
+    
+        }   
+
+
+}
